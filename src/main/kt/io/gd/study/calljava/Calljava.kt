0bbd8085764b708a9529117d55cd99bf5c9cf000
godong9 <godong9@gmail.com>
2020-06-01 08:14:18 +0900

    Update Calljava

--- src/main/kt/io/gd/study/calljava/Calljava.kt
+++ src/main/kt/io/gd/study/calljava/Calljava.kt
@@ -7,6 +7,23 @@ fun main() {
     car.color = "green"
     println(car)
 
+    car.variableMethod(5, "hello", "goodbye")
+    val strings = arrayOf("hello", "goodbye")
+    car.variableMethod(5, *strings)
+
+//    car.wantsIntArray(arrayOf(1, 2, 3).toIntArray())
+    car.wantsIntArray(intArrayOf(1, 2, 3))
+
+    println("x = ${Car.x}")
+    Car.x = 10
+    println(Car.xString())
+
+    println(car.demoMethod())
+    println(car.demoMethod( { println("I'm in a thread(kotlin)!") }))
+
+//    (car.anObject as java.lang.Object).notify()
+//    car.anObject.javaClass
+
     var model: String? = car.model
 //    println(model.javaClass)
     model = null
