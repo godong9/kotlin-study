e6ed05442b87527ee77eab9c40415941c950e17d
godong9 <godong9@gmail.com>
2020-06-16 07:31:23 +0900

    Update textadventure

---
+++ src/main/kt/io/gd/study/textadventure/Location.kt
@@ -0,0 +1,13 @@
+package io.gd.study.textadventure
+
+data class Location(val locationID: Int, val description: String) {
+    val exits = mutableMapOf<String, Int>()
+
+    init {
+        exits["Q"] = 0
+    }
+
+    fun addExit(direction: String, destinationID: Int) {
+        exits[direction] = destinationID
+    }
+}
